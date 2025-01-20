fd52a7c33cac6ce8403920d06dd9e22f902ce794
KimDowon <163387323+gun-kim@users.noreply.github.com>
2025-01-20 12:01:59 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main() {
+
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	std::cout.tie(0);
+
+	std::string S;
+	std::cin >> S;
+	std::cout << S.length();
+
+	return 0;
+}
