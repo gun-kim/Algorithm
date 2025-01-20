411f1b0f256de764f826cf4e9beb4beccc718197
KimDowon <163387323+gun-kim@users.noreply.github.com>
2025-01-20 12:01:02 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main() {
+
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	std::cout.tie(0);
+
+	std::string S;\
+	int i;
+	std::cin >> S >> i;
+	std::cout << S.at(i-1);
+
+	return 0;
+}
