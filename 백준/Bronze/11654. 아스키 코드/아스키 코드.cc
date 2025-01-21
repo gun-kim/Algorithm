35ed9bc6b2a4b27ea5cc0377b768663be716e21c
KimDowon <163387323+gun-kim@users.noreply.github.com>
2025-01-21 23:06:01 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main() {
+
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	std::cout.tie(0);
+
+  char str;
+  std::cin >> str;
+
+  std::cout << static_cast<int>(str);
+  
+	return 0;
+}
