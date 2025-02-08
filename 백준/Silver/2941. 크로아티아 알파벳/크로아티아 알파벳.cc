963c8b95f0d85f15c1f129dca4bd3e8408ee17a3
KimDowon <163387323+gun-kim@users.noreply.github.com>
2025-02-08 16:06:52 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+int count(std::string str);
+
+int main() {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
+
+  std::string word;
+  std::cin >> word;
+  std::cout << count(word);
+  return 0;
+}
+
+int count(std::string str)
+{
+  std::string alpa[8] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+  for (int i = 0;i < 8;i++)
+  {
+    while (1)
+    {
+      if (str.find(alpa[i]) != -1)
+      {
+        str.replace(str.find(alpa[i]), alpa[i].length(), "#");
+      }
+      else break;
+    }
+  }
+  return str.length();
+}
