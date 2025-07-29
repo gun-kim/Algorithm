c9d654839f77962dbfe88ace5561a3f205632e2d
KimDowon <163387323+gun-kim@users.noreply.github.com>
2025-07-29 16:50:00 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N, M; 
+	
+	cin >> N >> M;
+
+	int A[101][101];
+	int B[101][101];
+	
+	for(int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> A[i][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> B[i][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cout << A[i][j] + B[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
